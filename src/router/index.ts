4cdb650c4eb2204a040e2dfe1ece77f5e24c69db
wallyson ruan <wallysonruan@gmail.com>
2023-12-31 22:07:42 -0300

    fix: Vue components should always be named with two words

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/HomeView.vue";
-import Test from "@/views/Test.vue";
+import Test from "@/views/TestView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
