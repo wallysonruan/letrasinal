ed9c7af1641e64ae5ec55ae3400e68c6ed5b1b85
wallyson ruan <wallysonruan@gmail.com>
2024-01-02 00:31:12 -0300

    wip: building ToolBar for the new layout, check GoogleDocs for reference

--- src/components/ToolBar/ToolBar.vue
+++ src/components/ToolBar/ToolBar.vue
@@ -1,65 +1,20 @@
 <script setup lang="ts">
-import { ref } from "vue";
-
-import signPuddleSearchStore from "@/stores/SignPuddleStore";
-import pageStore from "@/stores/PageStore";
-
-const input = ref<string>();
-
-function clearInput() {
-  input.value = "";
-}
-
-function setInputed(text: string) {
-  pageStore().addItem(text);
-}
-
-function handleEvent(event: Event) {
-  if ((event as KeyboardEvent).key != "Enter" && event.type != "click") {
-    return;
-  }
-
-  if (input.value?.trim() == "") {
-    return;
-  }
-
-  setInputed(input.value as string);
-  clearInput();
-}
-
-const signPuddleSearch = signPuddleSearchStore();
 </script>
 <template>
-  <div class="toolbar-container">
-    {{ pageStore().items }}
+  <div class="toolbar-container no-printable">
     <div class="toolbar">
-      <v-text-field
-        v-model="input"
-        @keydown.enter="handleEvent"
-        label="Palavra, frase ou FSW"
-        variant="solo"
-      >
-        <template #append>
-          <v-btn
-            @click="signPuddleSearch.toggleSignPuddleSearch"
-            class="signpuddle-btn"
-            icon
-          >
-            <img src="@/assets/sign-puddle-icon.png" alt="SignPuddle" />
-          </v-btn>
-        </template>
-      </v-text-field>
+
     </div>
   </div>
 </template>
 <style scoped lang="scss">
 .toolbar-container {
+  position: sticky;
   .toolbar {
-    .signpuddle-btn {
-      img {
-        width: 2rem;
-      }
-    }
+    width: 90%;
+    height: 2rem;
+    border-radius: 1.5rem;
+    margin: 1rem auto;
   }
 }
 </style>
