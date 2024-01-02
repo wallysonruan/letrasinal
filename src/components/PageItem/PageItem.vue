69f5f9111b8c9c15e0c1297cb75e1ab91f05daaa
wallyson ruan <wallysonruan@gmail.com>
2024-01-02 16:28:45 -0300

    fix: border was adding distance between components

--- src/components/PageItem/PageItem.vue
+++ src/components/PageItem/PageItem.vue
@@ -13,18 +13,27 @@ const props = defineProps<PageItemProps>();
 </template>
 <style scoped lang="scss">
 .page-item {
+  position: relative;
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
-  padding: 1rem 0;
+  padding: 0.5rem 0;
   //
   overflow: auto; // resize need this
 
   &:hover {
     resize: both;
-    border: 1px solid rgb(0, 0, 0, 0.2);
-    border-radius: 0.3rem;
-    padding-bottom: 0%;
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      border: 2px solid #0d99ff;
+      border-radius: 0.3rem;
+    }
   }
 }
 </style>
