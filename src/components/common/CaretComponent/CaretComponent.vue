abd864d466e50d947a290c7737ec153b602be6c7
wallyson ruan <wallysonruan@gmail.com>
2024-01-21 03:21:54 -0300

    feat: caret is being pushed by Breakflow

--- src/components/common/CaretComponent/CaretComponent.vue
+++ src/components/common/CaretComponent/CaretComponent.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import pageStore from "../../../stores/PageStore";
+import { onMounted } from "vue";
 
 const activate = computed(() => {
   return pageStore().pageOnFocus;
@@ -8,6 +9,54 @@ const activate = computed(() => {
 const writingMode = computed(() => {
   return pageStore().getWritingConfiguration(1).writingMode;
 });
+
+function getCaretElement(): HTMLElement | null {
+  return document.querySelector(".caret") as HTMLElement | null;
+}
+
+function getCaretParentElement(caret: HTMLElement | null): HTMLElement | null {
+  return caret?.parentElement as HTMLElement | null;
+}
+
+function getPreviousSiblingOfElement(element: HTMLElement | null): HTMLElement | null {
+  return element?.previousElementSibling as HTMLElement | null;
+}
+
+function doesElementContainBreakflow(element: HTMLElement | null): boolean {
+  const childBreakflow = element?.querySelector(".breakflow");
+  return childBreakflow?.classList.contains("breakflow") || false;
+}
+
+function adjustCaretDimensions(caret: HTMLElement | null, isBigger: boolean): void {
+  if (!caret) {
+    return;
+  }
+
+  if (isBigger) {
+    caret.style.height = "6px";
+    caret.style.width = "6px";
+  } else {
+    caret.style.height = "0%";
+    caret.style.width = "0%";
+  }
+}
+
+// TODO: This is a temporary solution. It should be replaced with a more elegant one.
+// The problem is that since caret is an empty component using a pseudo-element to display it,
+// it doesn't have any dimensions. This causes the caret to be displayed in the wrong place, i.e.,
+// in the end of line/column being broken by Breakflow and not at the beginning of new line/column.
+function handleCaretAdjustmentOnMount(): void {
+  if (activate.value) {
+    const caret = getCaretElement();
+    const caretParent = getCaretParentElement(caret);
+    const caretParentSiblingBefore = getPreviousSiblingOfElement(caretParent);
+    const isSiblingBeforeBreakflowParent = doesElementContainBreakflow(caretParentSiblingBefore);
+
+    adjustCaretDimensions(caret, isSiblingBeforeBreakflowParent);
+  }
+}
+
+onMounted(handleCaretAdjustmentOnMount);
 </script>
 <template>
   <div v-show="activate" class="caret" :writing-mode="writingMode"></div>
