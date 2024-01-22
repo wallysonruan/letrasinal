fd4818c039f33e2940b8180e49c3eea12c35ac63
wallyson ruan <wallysonruan@gmail.com>
2024-01-22 02:48:39 -0300

    feat: store to manage all state related to PageItem only

---
+++ src/stores/PageItemStore.ts
@@ -0,0 +1,52 @@
+import { defineStore } from "pinia";
+import pageStore from "./PageStore";
+
+function isPageItemNearTopBorder(
+  pageItemId: string,
+  spaceNeededBetweenPageItemAndTopBorder: number = 0,
+) {
+  const pageItem = document.querySelector(
+    `.page-item[id="${pageItemId}"]`,
+  ) as HTMLElement;
+
+  if (!pageItem) {
+    console.log("PageItem not found");
+    return false;
+  }
+
+  return pageItem.offsetTop <= spaceNeededBetweenPageItemAndTopBorder;
+}
+
+function isPageItemNearRightBorder(
+  pageItemId: string,
+  spaceNeededBetweenPageItemAndRightBorder: number = 0,
+) {
+  const pageItem = document.querySelector(
+    `.page-item[id="${pageItemId}"]`,
+  ) as HTMLElement;
+
+  if (!pageItem) {
+    console.log("PageItem not found");
+    return false;
+  }
+
+  const pageItemRightBorderDistanceFromParentLeftBorder =
+    pageItem.offsetLeft + pageItem.offsetWidth;
+
+  return (
+    pageStore().getSheetSize(1).width -
+      pageItemRightBorderDistanceFromParentLeftBorder <=
+    spaceNeededBetweenPageItemAndRightBorder
+  );
+}
+
+const pageItemStore = defineStore({
+  id: "pageItemStore",
+  // state: () => ({}),
+  actions: {
+    isPageItemNearTopBorder,
+    isPageItemNearRightBorder,
+  },
+});
+
+export default pageItemStore;
