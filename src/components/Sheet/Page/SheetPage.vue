ce10dd738fcb30d538cb65e585a374a16890b26f
wallyson ruan <wallysonruan@gmail.com>
2024-01-31 13:54:27 -0300

    wip: pagination -> moved textarea onto Texteditor to be used by all pages

--- src/components/Sheet/Page/SheetPage.vue
+++ src/components/Sheet/Page/SheetPage.vue
@@ -21,132 +21,11 @@ const pageHeight = computed(() => {
     ? pageStore().getSheetSize(props.id).width
     : pageStore().getSheetSize(props.id).height;
 });
-
-function handleKeyDown(event: Event) {
-  const eventAsKeyboardEvent = event as KeyboardEvent;
-
-  const writingMode = computed(() => {
-    return pageStore().getWritingConfiguration(props.id).writingMode;
-  });
-
-  const isWritingModeVertical = writingMode.value === "vertical";
-
-  if (
-    (eventAsKeyboardEvent.key === "ArrowUp" ||
-      eventAsKeyboardEvent.key === "ArrowDown" ||
-      eventAsKeyboardEvent.key === "ArrowLeft" ||
-      eventAsKeyboardEvent.key === "ArrowRight" ||
-      eventAsKeyboardEvent.key === "Space" ||
-      eventAsKeyboardEvent.key === "Backspace" ||
-      eventAsKeyboardEvent.key === "Enter" ||
-      eventAsKeyboardEvent.key === "Tab") &&
-    pageStore().pageOnFocus.focus
-  ) {
-    event.preventDefault();
-  }
-
-  switch (eventAsKeyboardEvent.key) {
-    case "Backspace":
-      pageStore().deletePageItemBeforeCaret();
-      break;
-    case "Tab":
-      pageStore().addLongSpace();
-      break;
-    case "Enter":
-      pageStore().addBreakflow();
-      break;
-    case "ArrowUp":
-      if (isWritingModeVertical) {
-        pageStore().moveCaretUp();
-      }
-      break;
-    case "ArrowDown":
-      if (isWritingModeVertical) {
-        pageStore().moveCaretDown();
-      }
-      break;
-    case "ArrowLeft":
-      if (!isWritingModeVertical) {
-        pageStore().moveCaretUp();
-      }
-      break;
-    case "ArrowRight":
-      if (!isWritingModeVertical) {
-        pageStore().moveCaretDown();
-      }
-      break;
-    default:
-      return;
-  }
-}
-
-function handleInput(event: Event) {
-  const eventAsInputEvent = event as InputEvent;
-  const input = eventAsInputEvent.data;
-
-  if (
-    (input === " " ||
-      input === "," ||
-      input === "." ||
-      input === ":" ||
-      input === "!" ||
-      input === "?" ||
-      input === "(" ||
-      input === ")") &&
-    pageStore().pageOnFocus.focus
-  ) {
-    event.preventDefault();
-  }
-
-  if (input && /[0-9]/.test(input)) {
-    pageStore().addNumber(input);
-  }
-
-  switch (input) {
-    case " ":
-      pageStore().addSpace();
-      break;
-    case ",":
-      pageStore().addComma();
-      break;
-    case ".":
-      pageStore().addPeriod();
-      break;
-    case ":":
-      pageStore().addColon();
-      break;
-    case "!":
-      pageStore().addExclamationMark();
-      break;
-    case "?":
-      pageStore().addQuestionMark();
-      break;
-    case "(":
-      pageStore().addOpenParenthesis();
-      break;
-    case ")":
-      pageStore().addCloseParenthesis();
-      break;
-    default:
-      return;
-  }
-}
-
-function setFocusOnHiddenTextarea() {
-  const pageContainer = document.querySelector(".page-container");
-  const textarea = pageContainer?.querySelector("textarea");
-
-  if (textarea) {
-    textarea.focus({ preventScroll: true });
-  }
-}
 </script>
 <template>
   <div
     class="page"
     :pageId="props.id"
-    @click="setFocusOnHiddenTextarea"
-    @touchstart="setFocusOnHiddenTextarea"
     @dblclick="pageStore().placeCaretAtTheEnd"
   >
     <div
@@ -168,34 +47,13 @@ function setFocusOnHiddenTextarea() {
           :key="index"
         />
       </div>
-      <textarea
-        class="hidden-textarea"
-        @keydown="handleKeyDown"
-        @input="handleInput"
-        @focus="pageStore().setPageOnFocus(true, props.id)"
-        @focusout="pageStore().setPageOnFocus(false, props.id)"
-        aria-hidden="true"
-        aria-disabled="true"
-      >
-      <!-- Hidden. It's here just to get focus, toggle mobile virtual keyboard, and have its Events redirected to pagePage. -->
-    </textarea
-      >
     </div>
   </div>
 </template>
 <style scoped lang="scss">
 .page {
-  position: relative;
   padding: 0 1rem;
   overflow: scroll;
-
-  textarea {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 0%;
-    height: 0%;
-  }
 
   .page-container {
     padding: 1rem 1rem;
@@ -204,7 +62,6 @@ function setFocusOnHiddenTextarea() {
   }
 
   .page-content {
-    position: relative;
     display: flex;
     flex-wrap: wrap;
     align-content: baseline;
--- src/views/TextEditor/TextEditor.vue
+++ src/views/TextEditor/TextEditor.vue
@@ -6,6 +6,128 @@ import pageStore from "@/stores/PageStore";
 import { computed } from "vue";
 
 const pages = computed(() => pageStore().pages);
+const activePage = computed(() => pageStore().pageOnFocus.id);
+
+function handleKeyDown(event: Event) {
+  const eventAsKeyboardEvent = event as KeyboardEvent;
+
+  const writingMode = computed(() => {
+    return pageStore().getWritingConfiguration(activePage.value).writingMode;
+  });
+
+  const isWritingModeVertical = writingMode.value === "vertical";
+
+  if (
+    (eventAsKeyboardEvent.key === "ArrowUp" ||
+      eventAsKeyboardEvent.key === "ArrowDown" ||
+      eventAsKeyboardEvent.key === "ArrowLeft" ||
+      eventAsKeyboardEvent.key === "ArrowRight" ||
+      eventAsKeyboardEvent.key === "Space" ||
+      eventAsKeyboardEvent.key === "Backspace" ||
+      eventAsKeyboardEvent.key === "Enter" ||
+      eventAsKeyboardEvent.key === "Tab") &&
+    pageStore().pageOnFocus.focus
+  ) {
+    event.preventDefault();
+  }
+
+  switch (eventAsKeyboardEvent.key) {
+    case "Backspace":
+      pageStore().deletePageItemBeforeCaret();
+      break;
+    case "Tab":
+      pageStore().addLongSpace();
+      break;
+    case "Enter":
+      pageStore().addBreakflow();
+      break;
+    case "ArrowUp":
+      if (isWritingModeVertical) {
+        pageStore().moveCaretUp();
+      }
+      break;
+    case "ArrowDown":
+      if (isWritingModeVertical) {
+        pageStore().moveCaretDown();
+      }
+      break;
+    case "ArrowLeft":
+      if (!isWritingModeVertical) {
+        pageStore().moveCaretUp();
+      }
+      break;
+    case "ArrowRight":
+      if (!isWritingModeVertical) {
+        pageStore().moveCaretDown();
+      }
+      break;
+    default:
+      return;
+  }
+}
+
+function handleInput(event: Event) {
+  const eventAsInputEvent = event as InputEvent;
+  const input = eventAsInputEvent.data;
+
+  if (
+    (input === " " ||
+      input === "," ||
+      input === "." ||
+      input === ":" ||
+      input === "!" ||
+      input === "?" ||
+      input === "(" ||
+      input === ")") &&
+    pageStore().pageOnFocus.focus
+  ) {
+    event.preventDefault();
+  }
+
+  if (input && /[0-9]/.test(input)) {
+    pageStore().addNumber(input);
+  }
+
+  switch (input) {
+    case " ":
+      pageStore().addSpace();
+      break;
+    case ",":
+      pageStore().addComma();
+      break;
+    case ".":
+      pageStore().addPeriod();
+      break;
+    case ":":
+      pageStore().addColon();
+      break;
+    case "!":
+      pageStore().addExclamationMark();
+      break;
+    case "?":
+      pageStore().addQuestionMark();
+      break;
+    case "(":
+      pageStore().addOpenParenthesis();
+      break;
+    case ")":
+      pageStore().addCloseParenthesis();
+      break;
+    default:
+      return;
+  }
+}
+
+function setFocusOnHiddenTextarea(pageId: number) {
+  pageStore().pageOnFocus.id = pageId;
+
+  const pageContainer = document.querySelector(".text-editor-container");
+  const textarea = pageContainer?.querySelector("textarea");
+
+  if (textarea) {
+    textarea.focus({ preventScroll: true });
+  }
+}
 </script>
 <template>
   <div class="text-editor-container">
@@ -18,12 +140,26 @@ const pages = computed(() => pageStore().pages);
         :key="index"
         :id="page.pageId"
         :text="page.text"
+        @click="setFocusOnHiddenTextarea(page.pageId)"
+        @touchstart="setFocusOnHiddenTextarea(page.pageId)"
       />
+      <textarea
+        @keydown="handleKeyDown"
+        @input="handleInput"
+        @focus="pageStore().setPageOnFocus(true)"
+        @focusout="pageStore().setPageOnFocus(false)"
+        aria-hidden="true"
+        aria-disabled="true"
+      >
+      <!-- Hidden. It's here just to get focus, toggle mobile virtual keyboard, and have its Events redirected to pagePage. -->
+    </textarea
+      >
     </div>
   </div>
 </template>
 <style scoped lang="scss">
 .text-editor-container {
+  position: relative;
   background-color: rgb(128, 128, 128, 0.06);
   padding-top: 1.5rem;
   padding-bottom: 1.5rem;
@@ -43,6 +179,13 @@ const pages = computed(() => pageStore().pages);
     height: 100%;
     padding-top: 1rem;
   }
+}
+
+textarea {
+  position: sticky;
+  width: 50%;
+
+  margin: auto;
 }
 
 @media print {
